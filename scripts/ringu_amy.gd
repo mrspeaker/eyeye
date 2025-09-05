71e353a482f19a2d0676b6747381ebb9ae57ab0e
Mr Speaker <mrspeaker@gmail.com>
2025-09-05 08:41:59 +0800

    ringu moves for 5 seconds after player moves

--- scripts/ringu_amy.gd
+++ scripts/ringu_amy.gd
@@ -5,16 +5,20 @@ extends Node3D
 
 var start_pos: Vector3
 var moving = true
+var move_time = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	start_pos = position
 	SignalBus.connect("player_eyes_toggled", on_player_eyes)
+	SignalBus.connect("player_moved", on_player_moved)
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	if not moving or player == null:
+	if not moving or move_time <= 0 or player == null:
 		return
+		
+	move_time -= delta;
 	var offset = player.position - position
 
 	if offset.length() < 1.5:
@@ -26,6 +30,8 @@ func _physics_process(delta: float) -> void:
 	position.x += vel.x
 	position.z += vel.z
 
+func on_player_moved(player: CharacterBody3D):
+	move_time = 5.0
 
 func on_player_eyes(open: bool):
 	moving = open
