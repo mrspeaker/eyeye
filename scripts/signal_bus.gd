d1469700dc45ae736a2ffdb3920a9667d638ad73
Mr Speaker <mrspeaker@gmail.com>
2025-09-05 21:05:03 +0800

    suppress warnings

--- scripts/signal_bus.gd
+++ scripts/signal_bus.gd
@@ -1,7 +1,11 @@
 extends Node
 
+@warning_ignore("unused_signal")
 signal player_turned(player:CharacterBody3D)
+@warning_ignore("unused_signal")
 signal player_moved(player:CharacterBody3D)
+@warning_ignore("unused_signal")
 signal player_eyes_toggled(open: bool)
 
+@warning_ignore("unused_signal")
 signal interactable_scanned(interactable: Node3D)
