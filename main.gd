81a8ef50d3b8ea2b50f61008d0c6e45e61779a78
Mr Speaker <mrspeaker@gmail.com>
2025-09-06 21:23:18 +0800

    testing programatically changing tiles

--- main.gd
+++ main.gd
@@ -1,8 +1,20 @@
 extends Node3D
 
+@onready var grid:GridMap = %GridMap
+
+var _cell_timer:Timer = Timer.new()
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	SignalBus.player_moved.connect(world_turn)
+	
+	_cell_timer.wait_time = 1.0
+	_cell_timer.connect("timeout", self._on_timeout)
+	_cell_timer.autostart = true
+	add_child(_cell_timer)
+
+func _on_timeout():
+	grid.set_cell_item(Vector3i.ZERO, randi_range(0, 8))
 
 func _input(event):
 	if event.is_action_pressed("exit"):
