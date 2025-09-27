44d62fcf83686cd44f6eb92ce5b7c63e79174b55
Mr Speaker <mrspeaker@gmail.com>
2025-09-27 20:54:20 +0800

    more rejig

--- game/scripts/scarecrow.gd
+++ game/scripts/scarecrow.gd
@@ -1,6 +1,6 @@
 extends Node3D
 
-@onready var interact_label = get_node("../../UI/CanvasLayer/InteractLabel")
+@onready var interact_label = %InteractLabel #get_node("../../UI/CanvasLayer/InteractLabel")
 
 func interact(_entity:Node3D):
 	interact_label.text = "You'd be so scared if you were a crow."
