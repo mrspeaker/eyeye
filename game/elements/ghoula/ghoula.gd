6b99a043bd0a38aa7c880ea05c6048e76b87c8c3
Mr Speaker <mrspeaker@gmail.com>
2025-10-15 22:07:15 +0800

    reimport 3d ghoul

--- game/elements/ghoula/ghoula.gd
+++ game/elements/ghoula/ghoula.gd
@@ -4,4 +4,5 @@ extends Node3D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	look_at(target.position, Vector3.UP, true)
+	if target:
+		look_at(target.position, Vector3.UP, true)
