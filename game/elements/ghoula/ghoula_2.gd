1ab7639e696d0f84bfb50e5782135e71aafd771c
Mr Speaker <mrspeaker@gmail.com>
2025-10-16 10:45:59 +0800

    anim test

---
+++ game/elements/ghoula/ghoula_2.gd
@@ -0,0 +1,18 @@
+extends Node3D
+
+@export var target: Node3D
+
+var t:= 0.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+func _process(dt: float) -> void:
+	if target:
+		look_at(target.position, Vector3.UP, true)
+
+	t+=dt
+	if t > 5:
+		$AnimationPlayer.play("IdleTest" if Globals.rng.randf() < 0.5 else "GlitchLeft")
+		t -= 5
